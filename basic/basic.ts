ab7a46242d7f04ddf185bba46a58848802b53bad
노 기 훈 <66203019+Nohgh@users.noreply.github.com>
2025-04-28 07:53:31 +0900

    Init Project

---
+++ basic/basic.ts
@@ -0,0 +1,20 @@
+//기본 타입 예제
+let id: number = 5;
+let company: string = "Corp";
+let isPublished: boolean = true;
+let x: any = "string";
+
+//배열
+let ids: number[] = [12, 2, 3];
+let arr: any[] = [1, "a", true];
+
+//튜플
+let person: [number, string, boolean] = [1, "bard", true];
+
+//열거형
+enum Direction {
+  Up = 1,
+  Down,
+  Left,
+  Right,
+}
