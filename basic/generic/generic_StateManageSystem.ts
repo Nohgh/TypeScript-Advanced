900993be16b4b660fd1e0a4ddcb5186575dcb467
노 기 훈 <66203019+Nohgh@users.noreply.github.com>
2025-05-06 18:14:25 +0900

    Std: generic 상태관리 시스템

--- basic/generic/generic_StateManageSystem.ts
+++ basic/generic/generic_StateManageSystem.ts
@@ -1,1 +1,36 @@
 //복합 예제: 제너릭을 활용한 상태관리 시스템
+class StateManager<S> {
+  private state: S;
+
+  constructor(initialState: S) {
+    this.state = initialState;
+  }
+
+  getState(): S {
+    return this.state;
+  }
+
+  setState(newState: Partial<S>): void {
+    this.state = { ...this.state, ...newState };
+  }
+}
+
+interface UserState {
+  name: string;
+  age: number;
+  isLoggedIn: boolean;
+}
+
+const userStateManager = new StateManager<UserState>({
+  name: "Guset",
+  age: 0,
+  isLoggedIn: false,
+});
+
+console.log(userStateManager.getState());
+
+userStateManager.setState({ name: "noh", age: 25 });
+console.log(userStateManager.getState());
+
+userStateManager.setState({ isLoggedIn: true });
+console.log(userStateManager.getState());
