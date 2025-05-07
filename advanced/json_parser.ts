da5013417d4850100804a062e4cd5b6b4142bcb4
노 기 훈 <66203019+Nohgh@users.noreply.github.com>
2025-05-07 23:34:20 +0900

    Std: json parser

---
+++ advanced/json_parser.ts
@@ -0,0 +1,28 @@
+//타입스크립트의 재귀적 타입을 사용해 JSON 구조를 타입으로 표현
+type JSONValue =
+  | string
+  | number
+  | boolean
+  | null
+  | JSONValue[]
+  | { [key: string]: JSONValue };
+
+type JSONObject = { [key: string]: JSONValue };
+
+function parseJSON(json: string): JSONValue {
+  return JSON.parse(json);
+}
+
+function stringifyJSON(value: JSONValue): string {
+  return JSON.stringify(value);
+}
+
+//사용예
+const jsonString =
+  '{"name":"John","age":30,"isStudent":false,"hobbies":["reading","coding"]}';
+
+const parsed = parseJSON(jsonString);
+console.log(parsed);
+
+const stringified = stringifyJSON({ x: 5, y: [1, 2, 3] });
+console.log(stringified);
---
+++ react/ComponentProps.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ComponentProps = () => {
+  return <div>ComponentProps</div>;
+};
+
+export default ComponentProps;
