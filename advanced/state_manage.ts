e2c7820b86ea46b0a6f5b7dbe21b507bb56e8801
노 기 훈 <66203019+Nohgh@users.noreply.github.com>
2025-05-09 00:00:02 +0900

    Std: advanced

--- advanced/state_manage.ts
+++ advanced/state_manage.ts
@@ -1,1 +1,21 @@
 //인터섹션 타입과 유니온 타입을 이용한 복잡한 상태 관리
+type UserRole = "admin" | "user" | "guest";
+
+interface BaseState {
+  isLoading: boolean;
+  error: string | null;
+}
+
+interface GuestState extends BaseState {
+  userRole: "guest";
+}
+
+interface UserState extends BaseState {
+  userRole: "user";
+  username: string;
+}
+
+interface AdminState extends BaseState {
+  userRole: "admin";
+  username: string;
+}
